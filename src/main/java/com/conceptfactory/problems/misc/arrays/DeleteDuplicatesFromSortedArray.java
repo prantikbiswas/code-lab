930a63367d2d2b249d6edf0227cfa476d600a2cb
prantikbiswas <biswas.prantik@gmail.com>
2021-04-01 17:42:57 -0400

    1 Array problem

---
+++ src/main/java/com/conceptfactory/problems/misc/arrays/DeleteDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+package com.conceptfactory.problems.misc.arrays;
+
+public class DeleteDuplicatesFromSortedArray {
+
+    public int dedupe(int[] source) {
+        if (source.length == 0) { return 0; }
+
+        int lastElementPointer = 0;
+        for(int i = 1; i < source.length; i++) {
+            if (source[i] != source[lastElementPointer]) {
+                source[++lastElementPointer] = source[i];
+            }
+        }
+
+        return lastElementPointer + 1;
+    }
+
+}
---
+++ src/test/java/com/conceptfactory/problems/misc/arrays/DeleteDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,38 @@
+package com.conceptfactory.problems.misc.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteDuplicatesFromSortedArrayTest {
+
+    DeleteDuplicatesFromSortedArray uut = new DeleteDuplicatesFromSortedArray();
+
+    @Test
+    void dedupe() {
+        int[] input = new int[] { 1, 1, 2, 2, 3, 3, 3, 4, 5, 5, 6, 7 };
+
+        assertEquals(7, uut.dedupe(input));
+    }
+
+    @Test
+    void dedupe1() {
+        int[] input = new int[] { };
+
+        assertEquals(0, uut.dedupe(input));
+    }
+
+    @Test
+    void dedupe2() {
+        int[] input = new int[] { 1 };
+
+        assertEquals(1, uut.dedupe(input));
+    }
+
+    @Test
+    void dedupe3() {
+        int[] input = new int[] { 1, 1, 1, 1, 1, 1, 1, 1 };
+
+        assertEquals(1, uut.dedupe(input));
+    }
+}
